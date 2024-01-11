25988f8cf9e88ba56c5d25fda1836d15d93bdabd
Lodewijk Bogaards <lbogaards@stackstate.com>
2024-01-11 18:18:50 +0100

    Add first attempt at building a docker image using Nix

--- backend/flake.nix
+++ backend/flake.nix
@@ -10,6 +10,7 @@
     flake-utils.lib.eachDefaultSystem (system:
       let  
         pkgs = import nixpkgs { inherit system; overlays = [ ]; };
+        pkgsLinux = import nixpkgs { system = "x86_64-linux";  overlays = [ ]; };
         deps = pkgs: (with pkgs; [
           python311Full
           python311Packages.pip
@@ -23,7 +24,25 @@
           };
         };
         devShell = self.devShells."${system}".dev;
-      }
-    );
-}
 
+        apps.test = {
+          type = "app";
+          program = "<store-path>";
+        };
+
+        packages.dockerImage = pkgs.dockerTools.buildImage (builtins.trace "Building docker image" {
+          name = "etebeta-backend";
+          # tag = "latest";
+          copyToRoot = pkgs.buildEnv {
+              name = "foo";
+              paths = deps(pkgsLinux) ++ [
+                pkgsLinux.bash 
+                pkgsLinux.coreutils-full 
+              ];
+              pathsToLink = [ "/bin" ];
+          };
+        });
+      }
+   );
+
+}
